4be5e93cec6d557f797927e95e015e166096b19b
vinay sawant <sawantvinayr@gmail.com>
2012-07-15 23:53:26 +0530

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+BunkSheet
+
+This app is for bunkers who wants to keep track of bunked lectures.
+Developed by Vinay Sawant.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BunkSheet::Application.config.secret_token = 'ef02727ffe63e1354755522bc182b9660f67d3b5e1fe2b39ebe0c96f32857b6e09a464fa2a4150da0d680a9fca69c5c9a3778c6a6127e871638a9a59de9891e8'
