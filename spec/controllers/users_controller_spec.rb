7558462aef4197a9a8d6d9996ae905e56c3b8981
vinay sawant <sawantvinayr@gmail.com>
2012-07-19 01:40:48 +0530

    Finished with sign up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -52,4 +52,55 @@ describe UsersController do
 			response.should have_selector("title", :content => "Sign up")
 		end 
 	end 
+
+	describe "POST 'create'" do
+		
+		describe "failure" do
+			
+			before(:each) do
+			  @attr = {:name =>"",:email => "",:password => "",
+								 :password_confirmation => ""}
+			end
+
+			it "should have the right title" do
+				post :create, :user => @attr
+				response.should have_selector('title',:content => "Sign up")
+			end
+
+			it "should render the new page" do
+				post :create, :user => @attr
+				response.should render_template('new')
+			end
+
+			it "should not create a user" do
+				lambda do
+					post :create,:user => @attr
+				end.should_not change(User,:count)
+			end
+		end
+
+		describe "success" do
+	
+			before(:each) do
+			  @attr = {:name => "New User",:email => "example@gmail.com",
+								 :password => "footer",:password_confirmation => "footer"}
+			end	
+
+			it "should create a user" do
+				lambda do
+					post :create,:user => @attr
+				end.should change(User,:count).by(1)
+			end		
+
+			it "should redirect to user show page" do
+				post :create ,:user => @attr
+				response.should redirect_to(user_path(assigns(:user)))
+			end
+
+			it "should create a welcome message" do
+				post :create, :user => @attr
+				flash[:success].should =~ /welcome to the bunksheet/i
+			end
+		end
+	end
 end
