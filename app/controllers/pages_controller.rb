1400635a4baec9e1175076fca2e2dbcc125cb96f
vinay sawant <sawantvinayr@gmail.com>
2012-09-11 17:00:54 +0530

    added mechanism in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
 
   def home
 		@title="Home"
-		@current_schedule = current_user.schedules
+		if signed_in?
+			@current_schedule = current_user.schedules
+		end
   end
 
   def contact
