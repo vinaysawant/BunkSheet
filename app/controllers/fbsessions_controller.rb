12874683079af4869e7125f47af9dcd9fb39f433
vinay sawant <sawantvinayr@gmail.com>
2012-09-17 11:05:05 +0530

    new line

--- app/controllers/fbsessions_controller.rb
+++ app/controllers/fbsessions_controller.rb
@@ -2,8 +2,8 @@ class FbsessionsController < ApplicationController
   def create
     fbuser = Fbuser.from_omniauth(env["omniauth.auth"])
     session[:fbuser_id] = fbuser.id
-    redirect_to root_path, notice: 'Welcome to bunksheet \Follow these steps \1.First Create Your weekly schedule
-2.goto "settings => weekly schedule"'
+    redirect_to root_path, notice: 'Welcome to bunksheet \n***Follow these steps \n 1.First Create Your weekly schedule
+\n 2.goto "settings => weekly schedule"'
   end
 
   def destroy
